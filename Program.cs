b9afb491d838bb159fed8aa290b5a017fd8b8d3f
Supersmasher149 <walter.gibbons@gmail.com>
2020-12-02 22:02:33 -0500

    splits some things converts some things

--- Program.cs
+++ Program.cs
@@ -4,9 +4,29 @@ namespace day2
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            string[] lines = System.IO.File.ReadAllLines(@"C:\Users\walte\source\repos\day2\TextFile1.txt");
+            Console.WriteLine(lines[0]);
+            
+            //splits the min string
+            string[] min = lines[0].Split("-");
+            //splits the max string
+            string[] max = min[1].Split(" ");
+            //splits the character
+            string[] character = max[1].Split(":");
+            //splits the password
+             
+            
+            //convertes max string[] into int
+            int intMax = Int32.Parse(max[0]);
+            //converts min string[] into int
+            int intMin = Int32.Parse(min[0]);
+            //convert character into a string
+            string stringCharacter = character[0];
+
+            
+            Console.WriteLine(max[2]);
         }
     }
 }
